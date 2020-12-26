3b2a8d82a8ea2da9558d8162700e5808ec507653
tubasa.takei <fhleben.dntsygdby@gmail.com>
2020-12-26 12:59:22 +0000

    timeline

--- resources/views/user_follow/follow_button.blade.php
+++ resources/views/user_follow/follow_button.blade.php
@@ -1,6 +1,6 @@
 @if (Auth::id() != $user->id)
     @if (Auth::user()->is_following($user->id))
-        {!! Form::open(['route' => ['user.unfollow', $user->id, 'method' => 'delete']) !!}
+        {!! Form::open(['route' => ['user.unfollow', $user->id], 'method' => 'delete']) !!}
             {!! Form::submit('Unfollow', ['class' => "btn btn-danger btn-block"]) !!}
         {!! Form::close() !!}
     @else
